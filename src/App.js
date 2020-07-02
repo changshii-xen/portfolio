ddcf94b486a86546de70bb9469326455666572e3
Dominic <dominicwindvogel@gmail.com>
2020-07-02 17:17:19 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import 'materialize-css/dist/css/materialize.min.css';
+
+const App =() => {
+  return (
+    <div className="App">
+      My Portfolio
+    </div>
+  );
+}
+
+export default App;

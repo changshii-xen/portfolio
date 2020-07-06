752d563eae15a000dcf1ba09c0860cace0334128
Dominic <dominicwindvogel@gmail.com>
2020-07-06 10:07:51 +0200

    Updates

---
+++ precache-manifest.c73bee8baaa1e0bdcbb4065b27784e76.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29a7f05d696b59d169d034547411afa9",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "ef844a65150d416c087d",
+    "url": "/portfolio/static/css/2.36020323.chunk.css"
+  },
+  {
+    "revision": "2badc11fbd2b629e4b65",
+    "url": "/portfolio/static/css/main.e49b3f40.chunk.css"
+  },
+  {
+    "revision": "ef844a65150d416c087d",
+    "url": "/portfolio/static/js/2.8c7ee212.chunk.js"
+  },
+  {
+    "revision": "109083572dc3ba74f105a9f6f5c9e305",
+    "url": "/portfolio/static/js/2.8c7ee212.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2badc11fbd2b629e4b65",
+    "url": "/portfolio/static/js/main.6078a123.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "27de1c9c8caedded0bcbdd4ceba72eaf",
+    "url": "/portfolio/static/media/K3.27de1c9c.jpeg"
+  },
+  {
+    "revision": "382b11f4bbcb359aaa7d9d88031c0dde",
+    "url": "/portfolio/static/media/back.382b11f4.jpg"
+  },
+  {
+    "revision": "2491ecc296fa599ab6f187da6f2cdcc0",
+    "url": "/portfolio/static/media/main.2491ecc2.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.6a00133a62aaa8f97bcd54e563d57c24.js"
+  "/portfolio/precache-manifest.c73bee8baaa1e0bdcbb4065b27784e76.js"
 );
 
 self.addEventListener('message', (event) => {
